45f11ee9184793b134e8c85c78e5f029383d8281
JosephAlemu <aaleemuu@gmail.com>
2019-08-10 15:27:13 -0500

    services

---
+++ DVDManagement/Data/Repo/DvdRepo.cs
@@ -0,0 +1,38 @@
+﻿using DVDManagement.Data.context;
+using DVDManagement.Data.InterfaceRepo;
+using DVDManagement.Data.model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DVDManagement.Data.Repo
+{
+    public class DvdRepo : Repo<Dvd>, IDvdRepo
+    {
+        public DvdRepo(StoreDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Dvd> FindWithAuthor(Func<Dvd, bool> predicate)
+        {
+            return _context.Dvds
+                .Include(a => a.Author)
+                .Where(predicate);
+        }
+
+        public IEnumerable<Dvd> FindWithAuthorAndBorrower(Func<Dvd, bool> predicate)
+        {
+            return _context.Dvds
+                .Include(a => a.Author)
+                .Include(a => a.Customer)
+                .Where(predicate);
+        }
+
+        public IEnumerable<Dvd> GetAllWithAuthor()
+        {
+            return _context.Dvds.Include(a => a.Author);
+        }
+    }
+}
--- DVDManagement/Data/context/StoreDbContext.cs
+++ DVDManagement/Data/context/StoreDbContext.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using DVDManagement.Data.model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DVDManagement.Data.context
 {
-    public class StoreDbContext
+    public class StoreDbContext:DbContext
     {
+
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Dvd> Dvds { get; set; }
     }
+
+
+
 }
