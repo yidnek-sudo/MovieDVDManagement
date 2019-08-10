ed33b1b34cb22b7323f51b21d60b9544002a0dac
JosephAlemu <aaleemuu@gmail.com>
2019-08-10 12:53:06 -0500

    model class added

---
+++ DVDManagement/Data/model/Author.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DVDManagement.Data.model
+{
+    public class Author
+    {
+    }
+}
---
+++ DVDManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DVDManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
